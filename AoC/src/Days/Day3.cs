7d8c27afac79bafcebb094198ad91f49d0b7db80
Nixill <github@nixill.net>
2025-05-02 20:42:01 -0400

    Day 3 part 2 attempt 1

--- AoC/src/Days/Day3.cs
+++ AoC/src/Days/Day3.cs
@@ -9,14 +9,11 @@ public class Day3 : AdventDay
 {
   public override void Run(StreamReader input)
   {
-    IntVector2 position = new(0, 0);
-    Dictionary<IntVector2, int> presents = new Dictionary<IntVector2, int>
-    {
-      [(0, 0)] = 1
-    };
-    int maxCount = 1;
+    string directions = input.GetEverything();
+    IntVector2 position = (0, 0);
+    HashSet<IntVector2> presents = [(0, 0)];
 
-    foreach (char c in input.GetEverything())
+    foreach (char c in directions)
     {
       position += c switch
       {
@@ -26,11 +23,38 @@ public class Day3 : AdventDay
         'v' => IntVector2.Down,
         _ => throw new InvalidDataException()
       };
-      int count = presents.PlusOrSet(position, 1);
-      maxCount = int.Max(maxCount, count);
+      presents.Add(position);
     }
 
     Part1Number = presents.Count;
-    Part2Number = maxCount; // let's try and be clever and guess it :3
+
+    position = (0, 0);
+    presents = [(0, 0)];
+    IntVector2 roboPosition = (0, 0);
+
+    foreach (char[] c in directions.Chunk(2))
+    {
+      position += c[0] switch
+      {
+        '>' => IntVector2.Right,
+        '<' => IntVector2.Left,
+        '^' => IntVector2.Up,
+        'v' => IntVector2.Down,
+        _ => throw new InvalidDataException()
+      };
+      if (c.Length == 2)
+        roboPosition += c[1] switch
+        {
+          '>' => IntVector2.Right,
+          '<' => IntVector2.Left,
+          '^' => IntVector2.Up,
+          'v' => IntVector2.Down,
+          _ => throw new InvalidDataException()
+        };
+      presents.Add(position);
+      presents.Add(roboPosition);
+    }
+
+    Part2Number = presents.Count;
   }
 }

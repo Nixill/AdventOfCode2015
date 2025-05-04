e24be2944302ebdbbbeccef597c64cf5ec28b5d0
Nixill <github@nixill.net>
2025-05-04 15:29:31 -0400

    Day 3 optimization 1 attempt 1

--- AoC/src/Days/Day3.cs
+++ AoC/src/Days/Day3.cs
@@ -10,31 +10,15 @@ public class Day3 : AdventDay
   public override void Run(StreamReader input)
   {
     string directions = input.GetEverything();
-    IntVector2 position = (0, 0);
-    HashSet<IntVector2> presents = [(0, 0)];
-
-    foreach (char c in directions)
-    {
-      position += c switch
-      {
-        '>' => IntVector2.Right,
-        '<' => IntVector2.Left,
-        '^' => IntVector2.Up,
-        'v' => IntVector2.Down,
-        _ => throw new InvalidDataException()
-      };
-      presents.Add(position);
-    }
-
-    Part1Number = presents.Count;
-
-    position = (0, 0);
-    presents = [(0, 0)];
-    IntVector2 roboPosition = (0, 0);
+    IntVector2 positionP1 = (0, 0);
+    HashSet<IntVector2> presents1 = [(0, 0)];
+    IntVector2 santaP2 = (0, 0);
+    IntVector2 roboSantaP2 = (0, 0);
+    HashSet<IntVector2> presents2 = [(0, 0)];
 
     foreach (char[] c in directions.Chunk(2))
     {
-      position += c[0] switch
+      IntVector2 move = c[0] switch
       {
         '>' => IntVector2.Right,
         '<' => IntVector2.Left,
@@ -42,8 +26,14 @@ public class Day3 : AdventDay
         'v' => IntVector2.Down,
         _ => throw new InvalidDataException()
       };
+      positionP1 += move;
+      presents1.Add(positionP1);
+      santaP2 += move;
+      presents2.Add(santaP2);
+
       if (c.Length == 2)
-        roboPosition += c[1] switch
+      {
+        move = c[1] switch
         {
           '>' => IntVector2.Right,
           '<' => IntVector2.Left,
@@ -51,10 +41,14 @@ public class Day3 : AdventDay
           'v' => IntVector2.Down,
           _ => throw new InvalidDataException()
         };
-      presents.Add(position);
-      presents.Add(roboPosition);
+        positionP1 += move;
+        presents1.Add(positionP1);
+        roboSantaP2 += move;
+        presents2.Add(roboSantaP2);
+      }
     }
 
-    Part2Number = presents.Count;
+    Part1Number = presents1.Count;
+    Part2Number = presents2.Count;
   }
 }

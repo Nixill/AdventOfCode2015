038dd2f35a6ca96cc441db6808e298a48a6f1da8
Nixill <github@nixill.net>
2025-05-04 16:25:23 -0400

    Day 4 part 2 attempt 2

--- AoC/src/Days/Day4.cs
+++ AoC/src/Days/Day4.cs
@@ -8,6 +8,6 @@ public class Day4 : AdventDay
   {
     string code = input.GetEverything();
     Part1Number = Enumerable.Range(0, int.MaxValue).First(i => TextUtils.MD5HexHash($"{code}{i}").StartsWith("00000"));
-    if (!SkipPart2) Part2Number = Enumerable.Range((int)Part1Number, int.MaxValue).First(i => TextUtils.MD5HexHash($"{code}{i}").StartsWith("000000"));
+    if (!SkipPart2) Part2Number = Enumerable.Range((int)Part1Number, int.MaxValue - (int)Part1Number).First(i => TextUtils.MD5HexHash($"{code}{i}").StartsWith("000000"));
   }
 }

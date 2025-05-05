5f8a0c47111fcc8ced1e3b2d2897071bba1f89f6
Nixill <github@nixill.net>
2025-05-05 14:28:44 -0400

    Day 5 part 2 attempt 1

--- AoC/src/Days/Day5.cs
+++ AoC/src/Days/Day5.cs
@@ -4,13 +4,19 @@ namespace Nixill.AdventOfCode;
 
 public class Day5 : AdventDay
 {
+  // Part 1 requirements
   Regex Vowels = new(@"[aeiou]");
   Regex DoubleLetter = new(@"(.)\1");
   Regex BadGroups = new(@"(ab|cd|pq|xy)");
+
+  // Part 2 requirements
+  Regex DoublePair = new(@"(..).*\1");
+  Regex SinglePair = new(@"(.).\1");
 
   public override void Run(StreamReader input)
   {
     string[] list = input.GetAllLines();
     Part1Number = list.Count(l => Vowels.Matches(l).Count >= 3 && DoubleLetter.IsMatch(l) && !BadGroups.IsMatch(l));
+    Part2Number = list.Count(l => DoublePair.IsMatch(l) && SinglePair.IsMatch(l));
   }
 }

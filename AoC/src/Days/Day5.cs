633733ece595ccf0e7b5ce25cd818491c86df129
Nixill <github@nixill.net>
2025-05-05 15:28:11 -0400

    day 5 - static regexes

--- AoC/src/Days/Day5.cs
+++ AoC/src/Days/Day5.cs
@@ -5,13 +5,13 @@ namespace Nixill.AdventOfCode;
 public class Day5 : AdventDay
 {
   // Part 1 requirements
-  Regex Vowels = new(@"[aeiou]");
-  Regex DoubleLetter = new(@"(.)\1");
-  Regex BadGroups = new(@"(ab|cd|pq|xy)");
+  static Regex Vowels = new(@"[aeiou]");
+  static Regex DoubleLetter = new(@"(.)\1");
+  static Regex BadGroups = new(@"(ab|cd|pq|xy)");
 
   // Part 2 requirements
-  Regex DoublePair = new(@"(..).*\1");
-  Regex SinglePair = new(@"(.).\1");
+  static Regex DoublePair = new(@"(..).*\1");
+  static Regex SinglePair = new(@"(.).\1");
 
   public override void Run(StreamReader input)
   {

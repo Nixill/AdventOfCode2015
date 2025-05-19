ad35ee1b5914f155abf05b2027d6e648e6c109e7
Nixill <github@nixill.net>
2025-05-19 00:44:59 -0400

    Day 8 part 1 attempt 1

---
+++ AoC/src/Days/Day8.cs
@@ -0,0 +1,49 @@
+
+using Nixill.Utils.Extensions;
+
+namespace Nixill.AdventOfCode;
+
+public class Day8 : AdventDay
+{
+  public override void Run(StreamReader input)
+  {
+    string[] lines = input.GetAllLines();
+
+    Part1Number = lines.Select(l => l.Length - Parse(l).Length).Sum();
+  }
+
+  private string Parse(string input)
+    => new string(ParseEnum(input).ToArray());
+
+  private IEnumerable<char> ParseEnum(string input)
+  {
+    List<char> chars = input.Skip(1).SkipLast(1).ToList();
+
+    while (chars.Count > 0)
+    {
+      char c = chars.Pop();
+
+      if (c != '\\')
+      {
+        yield return c;
+        continue;
+      }
+
+      char c2 = chars.Pop();
+      if (c2 == '\\')
+      {
+        yield return '\\';
+      }
+      else if (c2 == '"')
+      {
+        yield return '"';
+      }
+      else if (c2 == 'x')
+      {
+        char c3 = chars.Pop();
+        char c4 = chars.Pop();
+        yield return (char)Convert.ToInt32(new string([c3, c4]), 16);
+      }
+    }
+  }
+}
